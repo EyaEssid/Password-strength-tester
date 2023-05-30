47e7a57e91cb2de8c12dc3a437eafecafe4a85b0
EyaEssid <135074356+EyaEssid@users.noreply.github.com>
2023-05-30 23:39:46 +0100

    Add files via upload

---
+++ pure python code.py
@@ -0,0 +1,120 @@
+import mysql.connector
+# Connect to the MySQL server
+conn = mysql.connector.connect(
+    user='root',
+    password='root',
+    host='127.0.0.1',
+    database='common_passwords',
+    auth_plugin='mysql_native_password'
+)
+
+# Create a cursor object to execute queries
+cursor = conn.cursor()
+
+# Execute the SELECT query
+cursor.execute("SELECT passwords FROM common_password;")
+
+# Fetch all the results
+results = cursor.fetchall()
+
+# Store the passwords in a list
+common_passwords = [row[0] for row in results]
+
+# Close the cursor and connection
+cursor.close()
+conn.close()
+
+import re
+
+def check_password_strength(username, email, birthdate, password):
+    # Check if the password exists in the common passwords list
+    if password.lower() in common_passwords:
+        return "Password is weak. It is a common password."
+
+    # Verify username, email, and birthdate existence in password
+    if (
+        username and any(part.lower() in password.lower() for part in username.split()) or
+        email and any(part.lower() in password.lower() for part in re.split(r"[\W_]+", email)) or
+        birthdate and any(part in password for part in re.findall(r"\d+", birthdate))
+    ):
+        complexity = 0
+        crack_time = "less than a second"
+        return f"Weak password. It contains parts of your username, email, or birthdate.\n password complexity = {complexity} \n needed time to crack = {crack_time}"
+
+    # Verify password strength based on criteria
+    has_length = len(password) >= 8
+    has_upper = re.search(r"[A-Z]", password) is not None
+    has_lower = re.search(r"[a-z]", password) is not None
+    has_digit = re.search(r"\d", password) is not None
+    has_symbol = re.search(r"[?!@#$%^&*():;,./_=+\-]", password) is not None
+
+    if has_length and has_upper and has_lower and has_digit and has_symbol:
+        complexity = 95 ** len(password)
+        crack_time = calculate_crack_time(complexity)
+        return f"Strong password, Complexity level = {complexity}, Time needed to crack: {crack_time}"
+    else:
+        complexity = 95 ** len(password)
+        feedback = []
+        if not has_length:
+            feedback.append("Password should have at least 8 characters.\n")
+        if not has_upper:
+            feedback.append("Password should have at least one uppercase letter.\n")
+        if not has_lower:
+            feedback.append("Password should have at least one lowercase letter.\n")
+        if not has_digit:
+            feedback.append("Password should have at least one digit.\n")
+        if not has_symbol:
+            feedback.append("Password should have at least one symbol (!@#$%^&*()).\n")
+
+        crack_time = calculate_crack_time(complexity)
+
+        return (
+            "Weak password. "
+            + ", ".join(feedback)
+            + f", Complexity level = {complexity}, Time needed to crack: {crack_time}"
+        )
+
+
+
+def calculate_crack_time(complexity):
+    # Estimate time to crack the password based on complexity
+    seconds_per_attempt = 1e-10  # Assuming 10 billion offline hash attempts per second
+    crack_time_seconds = complexity * seconds_per_attempt
+
+    crack_time_years = crack_time_seconds / (60 * 60 * 24 * 365)
+    crack_time_months = crack_time_seconds / (60 * 60 * 24 * 30)
+    crack_time_weeks = crack_time_seconds / (60 * 60 * 24 * 7)
+    crack_time_days = crack_time_seconds / (60 * 60 * 24)
+
+    years = int(crack_time_years)
+    months = int(crack_time_months % 12)
+    weeks = int(crack_time_weeks % 4)
+    days = int(crack_time_days % 7)
+    if crack_time_seconds < 1:
+        return "few seconds"  # Return a default value for short weak passwords
+
+    crack_time_str = ''
+    if years > 0:
+        crack_time_str += f'{years} year(s) '
+    if months > 0:
+        crack_time_str += f'{months} month(s) '
+    if weeks > 0:
+        crack_time_str += f'{weeks} week(s) '
+    if days > 0:
+        crack_time_str += f'{days} day(s)'
+
+    return crack_time_str.strip()
+
+
+
+# Example usage
+username = "John Doe"
+email = "johndoe@example.com"
+birthdate = "1980-01-01"
+password = "john2002"
+
+print(check_password_strength(username, email, birthdate, password))
+
+
+
+
